6cb9124a1ee72aea7949f56d9fcd62bf936e85f8
Manigandan C <manigandan.c2003@gmail.com>
2023-12-22 15:01:22 +0530

    Testing Forecast

--- dotnet6-api-sample/Program.cs
+++ dotnet6-api-sample/Program.cs
@@ -13,22 +13,22 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/health", () => "alive");
+app.MapGet("/weatherforecast", () => "alive");
 
 app.MapPost("/weatherfeed", () => "im here");
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 1).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-});
+// app.MapGet("/weatherforecast", () =>
+// {
+//     var forecast = Enumerable.Range(1, 1).Select(index =>
+//        new WeatherForecast
+//        (
+//            DateTime.Now.AddDays(index),
+//            Random.Shared.Next(-20, 55),
+//            summaries[Random.Shared.Next(summaries.Length)]
+//        ))
+//         .ToArray();
+//     return forecast;
+// });
 
 // Add Pathbase routing for AKS hosting
 var pathBase = builder.Configuration["PATH_BASE"];
